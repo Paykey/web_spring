12eeb085b4291bd6d5d6bd70d2332a9a2a5f8b42
Paykey <wltjd5827@naver.com>
2026-02-14 21:38:07 +0900

    Update, Delete 기능 추가

--- src/main/java/com/example/web_spring/HelloController.java
+++ src/main/java/com/example/web_spring/HelloController.java
@@ -28,4 +28,32 @@ public class HelloController
     {
         return postRepository.findAll();
     }
+
+    // 게시물 업데이트
+    @PutMapping("/posts/{id}")
+    public String updatePost(@PathVariable Long id, @RequestBody Post newPost)
+    {
+        //  해당 id의 게시물이 없으면 에러 메시지
+        Post post = postRepository.findById(id).orElseThrow(()->new IllegalArgumentException("E: 해당 ID의 글이 존재하지 않습니다."));
+
+        //  게시물 업데이트
+        post.setTitle(newPost.getTitle());
+        post.setContent(newPost.getContent());
+
+        //  교체 후 다시 저장
+        postRepository.save(post);
+
+        return "게시물을 수정하였습니다. 바뀐 제목: " + post.getTitle();
+    }
+
+    //  게시물 삭제
+    @DeleteMapping("/posts/{id}")
+    public String deletePost(@PathVariable Long id)
+    {
+        postRepository.deleteById(id);
+        return "ID: " + id + " 의 게시물이 삭제되었습니다.";
+    }
+
+
+
 }
